6ca6d50a9926f67be805de1af809dcca7eb06d27
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2024-07-31 11:55:48 +0330

    embed migrations

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,13 +1,18 @@
 package db
 
 import (
+	"embed"
+
 	_ "github.com/lib/pq"
 
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jmoiron/sqlx"
 )
+
+//go:embed migrations/*.sql
+var fs embed.FS
 
 func New(dsn string) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", dsn)
@@ -15,14 +20,12 @@ func New(dsn string) (*sqlx.DB, error) {
 		return nil, err
 	}
 
-	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+	source, err := iofs.New(fs, "migrations")
 	if err != nil {
 		return nil, err
 	}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://internal/db/migrations",
-		"postgres", driver)
+	m, err := migrate.NewWithSourceInstance("iofs", source, dsn)
 	if err != nil {
 		return nil, err
 	}
